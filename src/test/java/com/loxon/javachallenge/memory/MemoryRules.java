6e3b552b28474ed4388e0203ee376f627324bf09
mezricro <mezricro@gmail.com>
2019-10-27 22:45:56 +0100

    Final commit

--- src/test/java/com/loxon/javachallenge/memory/MemoryRules.java
+++ src/test/java/com/loxon/javachallenge/memory/MemoryRules.java
@@ -402,7 +402,7 @@ public class MemoryRules {
         game.nextRound( // allocate cells
             allocate(pA, 12, 15));
 
-        final Player pC = game.registerPlayer("command");
+        final Player pC = game.registerPlayer("c");
         assertResponse( // swap in a chain with 3 player
             game.nextRound(
                 swap(pB, 15, 13),
